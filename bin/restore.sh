ad4373c96b09e207ff72da1308ce1ee7a5a87bcf
Your Name <you@example.com>
2020-06-16 01:36:58 +0000

    updated restore

--- bin/restore.sh
+++ bin/restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone git@github.com:pry0cc/axiom-backup.git /tmp/backup
-rsync -avzr --include-from=/tmp/backup/backup-files.txt /tmp/backup/ /home/op
+rsync -avzr --include-from=/tmp/backup/.backup-files.txt /tmp/backup/ /home/op
